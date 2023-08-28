faa278d4ef2da71051639be1fba5445e1eee3efd
[헬스케어]김승혁 <tmdgur231@douzone.com>
2023-08-28 14:52:05 +0900

    [Remove] test component 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Blackjack from './pages/blackjack/Blackjack';
 
 export const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Blackjack />} />
-      </Routes>
-    </BrowserRouter>
+    // <BrowserRouter>
+    //   <Routes>
+    //     <Route path='/' element={< />} />
+    //   </Routes>
+    // </BrowserRouter>
+    <>
+      <h4>test webpack</h4>
+    </>
   );
 };
